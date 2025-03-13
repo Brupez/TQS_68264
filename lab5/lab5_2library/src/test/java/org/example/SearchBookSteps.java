df2ee0762c4e16cf9da3cd735c61ce3424c7212c
Bruno Lopes <brunolop50@gmail.com>
2025-03-13 18:05:31 +0000

    lab5.2 on the way

---
+++ lab5/lab5_2library/src/test/java/org/example/SearchBookSteps.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import io.cucumber.java.en.Given;
+
+public class SearchBookSteps {
+    @Given("a book with the title {string}, written by {string}, published in {int} March {int}")
+    public void aBookWithTheTitleOneGoodBookWrittenByAnonymousPublishedInMarch(int arg0, int arg1) {
+    }
+}
